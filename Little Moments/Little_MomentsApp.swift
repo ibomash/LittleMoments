40f4040ce4aa1bbd6735f52b666d6d45313e6e36
Illya Bomash <ibomash@gmail.com>
2023-05-01 07:36:42 -0400

    Initial Commit

---
+++ Little Moments/Little_MomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_MomentsApp.swift
+//  Little Moments
+//
+//  Created by Illya Bomash on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_MomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
