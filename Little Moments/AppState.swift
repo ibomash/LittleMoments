591dbc3699a75c12b2698b0053237920ff853e3e
Illya Bomash <ibomash@gmail.com>
2025-03-17 19:53:23 -0400

    chore(structure): Reorganize project following build architecture
    Implement file organization per build architecture document:
    - Move Swift files to feature-based directory structure
    - Organize resources in dedicated directories
    - Create Info.plist file in root directory
    - Update Xcode project references

--- Little Moments/AppState.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-class AppState: ObservableObject {
-  static let shared = AppState()
-  @Published var showTimerRunningView: Bool = false
-  @Published var showSettingsView: Bool = false
-}
